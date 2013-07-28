e5ccfa4e014d85a23b549bd7174053080f660f33
Wesley Victhor Mendes <w.v.mendes.s@gmail.com>
2013-07-28 15:39:20 -0300

    Learning jumps and movement.

--- main.lua
+++ main.lua
@@ -6,53 +6,54 @@ function love.load()
 
     world = love.physics.newWorld(0, earthGravity, true)
 
-    body = love.physics.newBody(world, screenWidth/2, screenHeight/2, 'dynamic')
-    bodyShape = love.physics.newRectangleShape(20, 20)
-    bodyFixture = love.physics.newFixture(body, bodyShape, 1)
+    player = {}
+    player.state = 'STOP'
+    player.body = love.physics.newBody(world, screenWidth/2 + 20, screenHeight/2 + 20, 'dynamic')
+    player.shape = love.physics.newRectangleShape(20, 20)
+    player.fixture = love.physics.newFixture(player.body, player.shape, 1)
 
-    body1 = love.physics.newBody(world, screenWidth/2 + 20, screenHeight/2 + 20, 'dynamic')
-    body1Shape = love.physics.newRectangleShape(20, 20)
-    body1Fixture = love.physics.newFixture(body1, body1Shape, 1)
-
-    body2 = love.physics.newBody(world, screenWidth/2 + 10, screenHeight/2 + 10, 'dynamic')
-    body2Shape = love.physics.newRectangleShape(20, 20)
-    body2Fixture = love.physics.newFixture(body2, body2Shape, 1)
+    player.body:setFixedRotation(true)
 
     floor = love.physics.newBody(world, screenWidth/2, (screenHeight/2) + 100, "static")
-    floorShape = love.physics.newRectangleShape(screenWidth, 20)
+    floorShape = love.physics.newRectangleShape(screenWidth, 1)
     love.physics.newFixture(floor, floorShape, 1)
 
-    bodyFixture:setFriction(1)
-    body1Fixture:setFriction(1)
-    body2Fixture:setFriction(1)
-
-    bodyFixture:setRestitution(1)
-    body1Fixture:setRestitution(1)
-    body2Fixture:setRestitution(1)
-
-    -- on set density to a fixture we must to reset its mass.
+    love.graphics.setBackgroundColor(61,61,61)
+    desiredVel = 0
 end
 
 function love.update(dt)
-    world:update(dt)
+    world:update(dt*10)
+
+    if love.keyboard.isDown("right") then
+        player.fixture:setFriction(2) 
+        player.body:applyLinearImpulse(5, 0)
+    elseif love.keyboard.isDown("left") then 
+        player.fixture:setFriction(2) 
+        player.body:applyLinearImpulse(-5, 0)
+    elseif love.keyboard.isDown("up") then 
+        player.fixture:setFriction(2) 
+        player.body:applyLinearImpulse(0, 0)
+    else
+        
+    end
 end
 
 function love.draw()
     love.graphics.setColor(255, 255, 255)
-    love.graphics.polygon('line', body:getWorldPoints(bodyShape:getPoints()))
+    love.graphics.polygon('fill', player.body:getWorldPoints(player.shape:getPoints()))
 
-    love.graphics.setColor(255, 222, 155)
-    love.graphics.polygon('line', body1:getWorldPoints(body1Shape:getPoints()))
-
-    love.graphics.setColor(222, 144, 255)
-    love.graphics.polygon('line', body2:getWorldPoints(body2Shape:getPoints()))
-
-    love.graphics.setColor(200, 220, 140)
+    love.graphics.setColor(000, 000, 000)
     love.graphics.polygon('fill', floor:getWorldPoints(floorShape:getPoints()))
 end 
 
 function love.keypressed(key)
     if key == "q" then
         os.exit(0)
+    end
+
+    if key == "i" then 
+        local impulse = player.body:getMass() * 40
+        player.body:applyLinearImpulse(0, -impulse)
     end
 end
